8d9399f5408417c86f15028d23c05fbb8a7ce2bf
Hannah Ritter <sibylbelle@gmail.com>
2013-10-30 18:38:33 -0400

    Added chapter 9

---
+++ homework6/Ch9/CriminalIntent/src/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -0,0 +1,61 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+//import android.widget.CheckBox;
+//import android.widget.CompoundButton;
+
+public class CrimeFragment extends Fragment {
+	private Crime mCrime; 
+	private EditText mTitleField; 
+	private Button mDateButton; 
+	//private CheckBox mSolvedCheckBox; 
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState); 
+		mCrime = new Crime(); 
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+		View v = inflater.inflate(R.layout.fragment_crime, parent, false);
+		
+		mTitleField = (EditText)v.findViewById(R.id.crime_title); 
+		mTitleField.addTextChangedListener(new TextWatcher() {
+			public void onTextChanged(CharSequence c, int start, int before, int count) {
+				mCrime.setTitle(c.toString());
+			}
+			
+			public void beforeTextChanged(CharSequence c, int start, int count, int after) {
+				//Intentional blank
+			}
+			
+			public void afterTextChanged(Editable c) {
+				//Blank
+			}
+		});
+		
+		mDateButton = (Button)v.findViewById(R.id.crime_date); 
+		mDateButton.setText(mCrime.getDate().toString()); 
+		mDateButton.setEnabled(false);
+		
+		/*mSolvedCheckBox = (CheckBox)v.findViewById(R.id.crime_solved); 
+		 mSolvedCheckBox.setOnCheckedChangeListener(new OnCheckedChangedListener() {
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				// set crime's solved property
+				mCrime.setSolved(isChecked); 
+			}
+		});
+		*/
+		return v; 
+	}
+
+}
